fef2a3d7e5a66ccaa0d79e3b3a3fcab03d31f0e9
DaryaZareM <daryaz2079@gmail.com>
2021-12-02 00:25:29 +0330

    ui done.

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("hello wooord!")
+
+
+var name = document.getElementById("fname").value;
+
+// var age = document.getElementById("age").value;
+// var fname = document.getElementById("fname").value;
+// submitOK = "true";
+
+// if (fname.length > 10) {
+//   alert("The name may have no more than 10 characters");
+//   submitOK = "false";
+// }
+
+// if (isNaN(age) || age < 1 || age > 100) {
+//   alert("The age must be a number between 1 and 100");
+//   submitOK = "false";
+// }
+
+// if (at == -1) {
+//   alert("Not a valid e-mail!");
+//   submitOK = "false";
+// }
+
+// if (submitOK == "false") {
+//   return false;
+// }
