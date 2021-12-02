f3e31bd92b40ba84a0bd734f2b6acb2b20be97e1
DaryaZareM <daryaz2079@gmail.com>
2021-12-02 12:57:56 +0330

    predict save and clear with local storage done!

--- index.js
+++ index.js
@@ -1,42 +1,68 @@
-console.log("hello wooord!")
+console.log("hello wooorld!")
 
 function saveFunction() {
+  console.log("save")
     var name = document.getElementById("fname").value;
-    
     submitOK = "true";
-
-    if (name.length > 255 && name.length < 2) {
+    if (name.length > 255 || name.length < 2) {
       alert("The name may have no more than 2 characters or more than 255 chars");
       submitOK = "false";
+
+      return false;
     }
+
+
+    myUrl="https://api.genderize.io/?name="+name;
+    res=httpGet(myUrl); 
+    //if rea is null //TODO
+    console.log(res);
+    var mydata = JSON.parse(res);
+    document.getElementById("probabilitylabel").innerHTML = mydata.probability;
+    document.getElementById("genderlabel").innerHTML = mydata.gender;
     
-    var gender=document.querySelector('input[name="gender"]:checked').value;
-    console.log(gender,name)
+    var gender;
+    if (document.getElementById('female').checked || document.getElementById('male').checked) {
+      gender=document.querySelector('input[name="gender"]:checked').value;
+      console.log(gender,name)
+      localStorage.setItem(name, gender);
+      document.getElementById("savedgenderlabel").innerHTML = gender;
+    }else{
+      document.getElementById("savedgenderlabel").innerHTML = mydata.gender;
+      gender= mydata.gender
+    }
+    localStorage.setItem( name,gender);
 
     if (submitOK == "false") {
       return false;
     }
-
     }
+
+
 function submitFunction() {
     console.log("submit")
 
     var name = document.getElementById("fname").value;
     submitOK = "true";
-    if (name.length > 255 && name.length < 2) {
+    if (name.length > 255 || name.length < 2) {
       alert("The name may have no more than 2 characters or more than 255 chars");
       submitOK = "false";
+
+      return false;
     }
-    console.log(name)
     myUrl="https://api.genderize.io/?name="+name;
     res=httpGet(myUrl); 
+    //if rea is null //TODO
     console.log(res);
 
     var mydata = JSON.parse(res);
     document.getElementById("genderlabel").innerHTML = mydata.gender;
     document.getElementById("probabilitylabel").innerHTML = mydata.probability;
 
-    document.getElementById("savedgenderlabel").innerHTML = mydata.gender;
+    if (localStorage.getItem(name)){
+      document.getElementById("savedgenderlabel").innerHTML = localStorage.getItem(name);
+      }else{
+      document.getElementById("savedgenderlabel").innerHTML = 'not saved';
+      }
 
     if (submitOK == "false") {
         return false;
@@ -44,7 +70,20 @@ function submitFunction() {
 
 }
 function clearFunction() {
-    dconsole.log("clear")
+    console.log("clear")
+    var name = document.getElementById("fname").value;
+    submitOK = "true";
+    if (name.length > 255 || name.length < 2) {
+      alert("The name may have no more than 2 characters or more than 255 chars");
+      submitOK = "false";
+
+      return false;
+    }
+
+    localStorage.removeItem( name);
+    document.getElementById("savedgenderlabel").innerHTML = '---';
+    
+
 }
 
 
